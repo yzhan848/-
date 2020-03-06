da9228ddc3610e041e254ca55e4e031361d805ce
雅文 <37919609+yzhan848@users.noreply.github.com>
2020-03-06 16:37:03 +0800

    Add files via upload

---
+++ community/src/main/resources/community/src/main/java/life/majiang/community/controller/IndexController.java
@@ -0,0 +1,41 @@
+package life.majiang.community.controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	private UserMapper userMapper;
+	
+	@GetMapping("/")
+	public String index(HttpServletRequest request) {
+		
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("token")) {
+				String token = cookie.getValue();
+				User user = userMapper.findByToken(token);
+				if (user != null) {
+					request.getSession().setAttribute("user",user);
+				}
+				break;
+			}
+		}
+		
+		
+		
+		return "index";
+	}
+
+}
